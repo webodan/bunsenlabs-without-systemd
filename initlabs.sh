3d5d7756bb85534457937874079e88d9001cf35a
webodan <danx91@hotmail.es>
2017-08-02 02:20:45 +0200

    Add files via upload

--- initlabs.sh
+++ initlabs.sh
@@ -15,7 +15,7 @@ echo "This script will require super-user privileges in order to make the follow
 echo "It will add the bl repos, their respective gpg key, install the required packages,"
 echo "Allow a user to reboot or shutdown the machine without super-user privileges,"
 echo "And then fix the bl-exit application so it shuts down using working sysvinit commands."
-echo "-----------------------"                      
+echo "------------------------"
 su
 echo "deb http://pkg.bunsenlabs.org/debian bunsen-hydrogen main" | tee -a /etc/apt/sources.list
 wget https://pkg.bunsenlabs.org/debian/pool/main/b/bunsen-keyring/bunsen-keyring_2016.7.2-1_all.deb
@@ -25,13 +25,4 @@ apt install openbox pulseaudio bunsen-meta-all
 apt purge xfce4-notifyd
 chmod 777 /sbin/shutdown
 cp bl-exit /usr/bin/bl-exit
-echo "We need to install your GPU driver. Do you have ATi/AMD (a) or NVIDIA (n)?"
-while true; do
-read -rsn1 input
-if [ "$input" = "a" ]; then
-apt install xserver-xorg-video-radeon xserver-xorg-video-ati
-elif ["$input" = "n" ]; then
-apt install xserver-xorg-video-nvidia xserver-xorg-video-nouveau
-fi
-echo "Done. Please reboot if the drivers were installed successfully."
-exit
+echo "Done."
